6b45b3997fce0f269b92b03751edd3f6f2be3263
PuDyDg <phuduydang055@gmail.com>
2026-05-23 08:50:49 +0700

    Update VietMed NER PhoBERT training config

--- vietmed_ner_phobert.sh
+++ vietmed_ner_phobert.sh
@@ -8,9 +8,9 @@ run(){
   l_num=$3
   seed=42
   learning_rate=3e-5
-  num_train_epochs=10
-  batch_size=16
-  lora_alpha=16
+  num_train_epochs=60
+  batch_size=64
+  lora_alpha=64
   lora_dropout=0.05
   target_modules="query value"
   wandb_project=project_name
